27547b9d183f212af86bf50c48de698c20fb1e31
ekdma1458 <52541586+ekdma1458@users.noreply.github.com>
2020-01-20 19:23:32 +0900

    Modify FlowC operator<

--- FlowC.cpp
+++ FlowC.cpp
@@ -13,7 +13,7 @@ FlowC::FlowC(ST_JE_TCP_Packet *packet, bool check){
     }
 }
 bool FlowC::operator<(const FlowC& other) const{
-    return memcmp(this, &other, sizeof (FlowC));
+    return memcmp(this, &other, sizeof (FlowC)) < 0;
 }
 void FlowC::toString(){
     printf("src_ip : %d.%d.%d.%d\r\n", (ntohl(src_ip) & 0xff000000) >> 24  , (ntohl(src_ip) & 0x00ff0000) >> 16 , (ntohl(src_ip) & 0x0000ff00) >> 8 , ntohl(src_ip) & 0x000000ff);
